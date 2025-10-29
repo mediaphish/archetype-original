213037ac12315a7e81b65d267b48e674e6edc27b
mediaphish <mediaphish@users.noreply.github.com>
2025-10-29 18:23:50 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ import Hero from "./components/Hero.jsx";
 import ChatApp from "./app/ChatApp.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
-import WhatWeDo from "./components/WhatWeDo.jsx";
 import Journal from "./pages/Journal.jsx";
-import JournalPost from "./pages/JournalPost.jsx";
-import MentoringConsulting from "./pages/MentoringConsulting.jsx";
-import SpeakingWorkshops from "./pages/SpeakingWorkshops.jsx";
-import FractionalLeadership from "./pages/FractionalLeadership.jsx";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('home');
@@ -20,14 +15,6 @@ export default function App() {
       const path = window.location.pathname;
       if (path === '/journal') {
         setCurrentPage('journal');
-      } else if (path.startsWith('/journal/')) {
-        setCurrentPage('journal-post');
-      } else if (path === '/mentoring-consulting') {
-        setCurrentPage('mentoring-consulting');
-      } else if (path === '/speaking-workshops') {
-        setCurrentPage('speaking-workshops');
-      } else if (path === '/fractional-leadership') {
-        setCurrentPage('fractional-leadership');
       } else {
         setCurrentPage('home');
       }
@@ -40,52 +27,12 @@ export default function App() {
     return () => window.removeEventListener('popstate', handleRoute);
   }, []);
 
-  // Render Journal post page
-  if (currentPage === 'journal-post') {
-    return (
-      <main className="bg-white text-black">
-        <Header />
-        <JournalPost />
-      </main>
-    );
-  }
-
   // Render Journal page
   if (currentPage === 'journal') {
     return (
       <main className="bg-white text-black">
         <Header />
         <Journal />
-      </main>
-    );
-  }
-
-  // Render Mentoring & Consulting page
-  if (currentPage === 'mentoring-consulting') {
-    return (
-      <main className="bg-white text-black">
-        <Header />
-        <MentoringConsulting />
-      </main>
-    );
-  }
-
-  // Render Speaking & Workshops page
-  if (currentPage === 'speaking-workshops') {
-    return (
-      <main className="bg-white text-black">
-        <Header />
-        <SpeakingWorkshops />
-      </main>
-    );
-  }
-
-  // Render Fractional Leadership page
-  if (currentPage === 'fractional-leadership') {
-    return (
-      <main className="bg-white text-black">
-        <Header />
-        <FractionalLeadership />
       </main>
     );
   }
@@ -97,7 +44,6 @@ export default function App() {
       <Hero />
       <ChatApp />
       <About />
-      <WhatWeDo />
       <Contact />
     </main>
   );
