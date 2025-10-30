93fb5779f42b4c52b3d6d1c44ab1f8776eb2d337
mediaphish <mediaphish@users.noreply.github.com>
2025-10-29 21:13:54 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
+import SEO from "./components/SEO.jsx";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
 import ChatApp from "./app/ChatApp.jsx";
 import About from "./components/About.jsx";
+import Philosophy from "./components/Philosophy.jsx";
+import Methods from "./components/Methods.jsx";
 import Contact from "./components/Contact.jsx";
 import Journal from "./pages/Journal.jsx";
+import AboutPage from "./pages/About.jsx";
+import PhilosophyPage from "./pages/Philosophy.jsx";
+import MethodsPage from "./pages/Methods.jsx";
+import WhatWeDoPage from "./pages/WhatWeDo.jsx";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState('home');
@@ -15,6 +22,14 @@ export default function App() {
       const path = window.location.pathname;
       if (path === '/journal') {
         setCurrentPage('journal');
+      } else if (path === '/about') {
+        setCurrentPage('about');
+      } else if (path === '/philosophy') {
+        setCurrentPage('philosophy');
+      } else if (path === '/methods') {
+        setCurrentPage('methods');
+      } else if (path === '/what-we-do') {
+        setCurrentPage('what-we-do');
       } else {
         setCurrentPage('home');
       }
@@ -26,6 +41,46 @@ export default function App() {
 
     return () => window.removeEventListener('popstate', handleRoute);
   }, []);
+
+  // Render About page
+  if (currentPage === 'about') {
+    return (
+      <main className="bg-white text-black">
+        <Header />
+        <AboutPage />
+      </main>
+    );
+  }
+
+  // Render Philosophy page
+  if (currentPage === 'philosophy') {
+    return (
+      <main className="bg-white text-black">
+        <Header />
+        <PhilosophyPage />
+      </main>
+    );
+  }
+
+  // Render Methods page
+  if (currentPage === 'methods') {
+    return (
+      <main className="bg-white text-black">
+        <Header />
+        <MethodsPage />
+      </main>
+    );
+  }
+
+  // Render What We Do page
+  if (currentPage === 'what-we-do') {
+    return (
+      <main className="bg-white text-black">
+        <Header />
+        <WhatWeDoPage />
+      </main>
+    );
+  }
 
   // Render Journal page
   if (currentPage === 'journal') {
@@ -39,12 +94,17 @@ export default function App() {
 
   // Render Home page
   return (
-    <main className="bg-white text-black">
-      <Header />
-      <Hero />
-      <ChatApp />
-      <About />
-      <Contact />
-    </main>
+    <>
+      <SEO pageKey="default" />
+      <main className="bg-white text-black">
+        <Header />
+        <Hero />
+        <ChatApp />
+        <About />
+        <Philosophy />
+        <Methods />
+        <Contact />
+      </main>
+    </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import "./styles/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
