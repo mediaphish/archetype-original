4cff8e152c356e3784e7ecd9c246be69fc567592
Bart Paden <mediaphish@macbookpro.lan>
2026-01-22 09:18:30 -0600

    Fix Header.jsx: remove extra closing div tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -333,7 +333,6 @@ export default function Header() {
                 </div>
               </div>
             </div>
-            </div>
 
             {/* Mobile Menu Button - Animated Hamburger to X */}
             <button 
